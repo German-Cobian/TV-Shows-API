2f59065166977bf8c075beff9d9b366228d3679e
German Renato Cobian <germancobian@hotmail.com>
2023-07-10 17:50:03 -0700

    Implement search-bar and getTvShows API to fetch tv-shows according to input criteria

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ logo.src = Logo;
 
 const collage = document.getElementById('collage');
 collage.src = Collage;
+
+// Tv-shows API calls
+
+const getTvShows = async (searchString) => {
+  const URL = `https://api.tvmaze.com/search/shows?q=${searchString}`;
+  const results = await fetch(`${URL}`);
+  const tvShows = await results.json();
+  console.log(tvShows);
+};
+
+// Search bar
+
+document.getElementById('search-bar').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchString = document.getElementById('search-category').value;
+  searchString.toLowerCase();
+  getTvShows(searchString);
+  console.log(searchString);
+});
---
+++ src/search.js
@@ -0,0 +1,3 @@
+// Tv-shows API calls
+
+
